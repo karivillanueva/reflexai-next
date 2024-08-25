ea62760dde33343bd98c0652ac6d357078d4044f
karivillanueva <karimevillanueva96@gmail.com>
2024-08-25 12:23:29 -0300

    Create admin page UI

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,37 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { getAllChats } from '../api/chats';
+import { ChatType } from '../types/ChatType';
+import ChatSummary from '../components/ChatSummary';
+
 const AdminDashboard = () => {
+  const [chats, setChats] = useState<ChatType[]>([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const allChats = await getAllChats();
+        setChats(allChats);
+      } catch (error) {
+        console.error('Error fetching chats:', error);
+      }
+    };
+
+    fetchChats();
+  }, []);
+
   return (
     <div className="p-4 min-h-screen flex flex-col">
-      This is the admin dashboard
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Administration Dashboard
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {chats.map((chat) => (
+          <ChatSummary chat={chat} key={chat.id} />
+        ))}
+      </div>
     </div>
   );
 };
+
 export default AdminDashboard;
